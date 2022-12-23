e207afaeeab6e777530025fba7a0b371c4c16271
AdiUnique08 <118947155+AdiUnique08@users.noreply.github.com>
2022-12-23 13:01:51 +0530

    Add files via upload

--- PYTHON PROJECT.py
+++ PYTHON PROJECT.py
@@ -17,11 +17,13 @@ def disease():
         elif dis.lower() == dis_list1[3]:
             print("\nApply Zandu Balm")
         elif dis.lower() == dis_list1[4]:
-            print("Make use of Albuterol")
+            print("\nMake use of Albuterol")
         elif dis.lower() == dis_list1[5]:
-            print("Take a painkiller")
+            print("\nTake a painkiller")
         elif dis.lower() == dis_list1[6]:
-            print("Make use of eyedrops")
+            print("\nMake use of eyedrops")
+        elif dis.lower() == dis_list1[7]:
+            print("\nTake Antibiotic tablets ")
     elif dis.lower() in dis_list2:
         if dis.lower() == dis_list2[0]:
             print("\nPlease consult an Oncologist")
@@ -35,6 +37,8 @@ def disease():
             print("\nPlease consult a Endocrinologist")
         elif dis.lower() == dis_list2[5]:
             print("\nPlease consult a Pulmonologist")
+        elif dis.lower() == dis_list2[6]:
+            print("\nPlease consult a Dentist")
     elif dis.lower() not in dis_list1 and dis.lower() not in dis_list2:
         print("\nThe entered medical issue is currently not available.")
         print("\nSorry for the inconvenience caused")
@@ -43,8 +47,8 @@ def disease():
 
 a = 1
 while a == 1:
-    dis_list1 = ['cold', 'fever', 'cough', 'headaches','asthama',"bodypain",'conjunctivitis']
-    dis_list2 = ['cancer', 'pneumonia', 'stroke', 'arthritis','Tuberculosis','Diabetes',' pulmonary disease']
+    dis_list1 = ['cold', 'fever', 'cough', 'headaches','asthama',"bodypain",'conjunctivitis','maleria']
+    dis_list2 = ['cancer', 'pneumonia', 'stroke', 'arthritis','Tuberculosis','Diabetes',' pulmonary disease','toothache']
     name = input("Enter your name: ")
     age = int(input("Enter your age: "))
     disease()
